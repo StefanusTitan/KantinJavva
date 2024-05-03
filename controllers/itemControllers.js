9d0526786897d9cb7800f224ab97bf951a609595
Jonathan_HA78 <joemaxwell789@gmail.com>
2024-05-03 21:42:07 +0700

    adding multer and upload pict on item

--- controllers/itemControllers.js
+++ controllers/itemControllers.js
@@ -1,4 +1,6 @@
 const { Item, Category } = require("../models/itemModel");
+const upload = require('../middleware/multer');
+
 
 exports.getAllMenuItems = async (req, res) => {
     try {
@@ -38,12 +40,16 @@ exports.updateCategory = async (req, res) => {
     }
 }
 
-exports.createItem = async (req, res) => {
+exports.createItem = upload.single('image'), async (req, res) => {
+    // Access the uploaded file using req.file
+    // Process your item creation logic here
+    // For example:
     const item = new Item({
         name: req.body.name,
         price: req.body.price,
         description: req.body.description,
-        category: req.body.category
+        category: req.body.category,
+        image: req.file.path // Assuming your item model has an 'image' field to store the file path
     });
 
     try {
@@ -53,6 +59,30 @@ exports.createItem = async (req, res) => {
         res.status(400).json({ message: error.message });
     }
 }
+
+exports.updateItem = upload.single('image'), async (req, res) => {
+    try {
+        const item = await Item.findById(req.params.id);
+        if (item) {
+            item.name = req.body.name || item.name;
+            item.price = req.body.price || item.price;
+            item.description = req.body.description || item.description;
+            item.category = req.body.category || item.category;
+            if (req.file) {
+                item.image = req.file.path; // Update the image path if a new image is uploaded
+            }
+
+            const updatedItem = await item.save();
+            res.status(200).json({ message: 'Item updated successfully', data: updatedItem });
+        } else {
+            res.status(404).json({ message: 'Item not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+
 
 // Delete an item
 exports.deleteItem = async (req, res) => {
@@ -70,7 +100,7 @@ exports.deleteItem = async (req, res) => {
 }
 
 // Update an item
-exports.updateItem = async (req, res) => {
+exports.updateItem = upload.single('image'), async (req, res) => {
     try {
         const item = await Item.findById(req.params.id);
         if (item) {
@@ -78,6 +108,9 @@ exports.updateItem = async (req, res) => {
             item.price = req.body.price || item.price;
             item.description = req.body.description || item.description;
             item.category = req.body.category || item.category;
+            if (req.file) {
+                item.image = req.file.path; // Update the image path if a new image is uploaded
+            }
 
             const updatedItem = await item.save();
             res.status(200).json({ message: 'Item updated successfully', data: updatedItem });
